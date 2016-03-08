ddb2df8c1fcc56224bcf9b4bf86629a8cde862cb
tktr <no.82.vuf.lab@gmail.com>
2016-03-08 15:30:32 +0900

    finish top page

--- js/common.js
+++ js/common.js
@@ -1,12 +1,18 @@
-function writeHtml(parts){
-
-    $.ajax({
-	url: parts + ".html",
-	cache: false,
-	async: false,
-	success: function(html){
-	    document.write(html);
-	}
+(function(window, $) {
+    'use strict';
+    $(function() {
+	//affix
+	setTimeout(function() {
+            $('.fix-sidebar').affix({
+		offset: {
+		    top: function() {
+			return (this.top = $('header').outerHeight(true));
+		    },
+		    bottom: function() {
+			return (this.bottom = $('footer').outerHeight(true));
+		    }
+		}
+            })
+	}, 100);
     });
-
-}
+})(window, jQuery);
